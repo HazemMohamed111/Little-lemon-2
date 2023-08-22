740c0282869a9ad3f1dad6b1d0d711fce4d30eb3
Hazem Mohamed <hazemhyz12@gmail.com>
2023-08-22 18:52:57 +0200

    hazen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from'../src/components/Header.js';
+import Footer from'../src/components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+  <>
+   <Header />;
+   <Footer />;
+
+
+  </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import LOGO1 from "../Logo.png"
+function Footer() {
+    return (
+        <footer>
+        <div> 
+            <ul>
+                <img src={LOGO1} alt="Logo"></img>
+                 <li><p>navigation</p></li>
+                 <li><a href='/home'>home</a></li>
+                 <li><a href='./about'>specials</a></li>
+                 <li><a href='./special'>Testimonials</a></li>
+                 <li><a href='./rservation'>About</a></li>
+            </ul>
+      </div>
+      <div>
+        <ul>
+            <li><p>Contacts</p></li>
+            <li><a href='./Address'>Address</a></li>
+            <li><a href='./Phone'>Phone</a></li>
+            <li><a href='./Email'>Email</a></li>
+        </ul>
+      </div>
+      <div>
+            <ul>
+                <li><p>Social Media</p></li>
+                <li><a href='./Address'>Facebook</a></li>
+                <li><a href='./Phone'>instagrm</a></li>
+                <li><a href='./Email'>Whats app</a></li>
+            </ul>
+      </div>
+      </footer>
+    );
+
+}
+export default Footer
